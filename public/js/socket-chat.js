f0fc17946f579f95f2563632f2f0f1248ab6705a
José Luis Vilches <jose.luis.comvilches@gmail.com>
2021-05-14 01:37:23 -0300

    Agregamos un template de front end para el chat

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,9 +1,10 @@
 var socket = io()
+
 var params = new URLSearchParams(window.location.search)
 
-if(!params.has('nombre') || !params.has('sala')){
+if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html'
-    throw new Error('Se necesita un nombre y una sala')
+    throw new Error('El nombre y sala son necesarios')
 }
 
 var usuario = {
@@ -11,37 +12,33 @@ var usuario = {
     sala: params.get('sala')
 }
 
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor')
 
-    socket.emit('entrarChat', usuario, function(resp){
+    socket.emit('entrarChat', usuario, function(resp) {
         console.log('Usuarios conectados', resp)
     })
+
 })
 
 socket.on('disconnect', function() {
 
     console.log('Perdimos conexión con el servidor')
-})
 
-/*socket.emit('crearMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp)
-})*/
+})
 
 socket.on('crearMensaje', function(mensaje) {
-
     console.log('Servidor:', mensaje)
-
 })
 
-socket.on('listaPersonas', function(personas) {
-
+socket.on('listaPersona', function(personas) {
     console.log(personas)
 })
 
-socket.on('mensajePrivado', function(mensaje){
-    console.log('Mensaje privado', mensaje)
+socket.on('mensajePrivado', function(mensaje) {
+
+    console.log('Mensaje Privado:', mensaje)
+
 })
